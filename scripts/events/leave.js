ff4b203be1c36d239da9a0cba92516e9a7516af0
Mierukochannn <anjara0003@gmail.com>
2025-06-08 16:38:45 +0300

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -3,7 +3,7 @@ const { getTime, drive } = global.utils;
 module.exports = {
   config: {
     name: "leave",
-    version: "1.4",
+    version: "1.5",
     author: "aesther",
     category: "events"
   },
@@ -25,7 +25,7 @@ module.exports = {
       session4: "evening",
       leaveType1: "(￢ ￢) 𝗟𝗘𝗙𝗧",
       leaveType2: "__φ(．．;)𝚠𝚊𝚜 𝗞𝗜𝗖𝗞𝗘𝗗 𝚏𝚛𝚘𝚖",
-      defaultLeaveMessage: "{userName} {type} 𝚃𝚑𝚎 𝙶𝚛𝚘𝚞𝚙 [🥶]"
+      defaultLeaveMessage: "{userName} {type} 𝚃𝚑𝚎 𝙶𝚛𝚘𝚞𝚙 [🪐]"
     }
   },
 
