515be64f230c5fc7db2fd29d6ce42ea05eb3b4ac
termo-furtif <terminatorfurtif@gmail.com>
2025-06-27 08:52:52 +0000

    Add files via upload

---
+++ scripts/cmds/dalle.js
@@ -0,0 +1,54 @@
+const axios = require('axios');
+const fs = require('fs-extra');
+const path = require('path');
+
+module.exports = {
+  config: {
+    name: "dalle3",
+    aliases: ["dalle","bing","create"],
+    version: "1.0",
+    author: "Dipto",
+    countDown: 15,
+    role: 0,
+    shortDescription: "Generate images powerby by Dalle3",
+    longDescription: "Generate images by Unofficial Dalle3",
+    category: "download",
+    guide: {
+      en: "{pn} prompt"
+    }
+  },
+
+  onStart: async function ({ api, event, args }) {
+  const prompt = event.messageReply?.body.split("dalle")[1] ||  args.join(" ");
+  if (!prompt) {
+   return api.sendMessage("❌| Wrong Formet .✅ | Use 17/18 years old boy/girl watching football match on tv and written Dipto and 69 on the back of his Dress , 4k",event.threadID,event.messageID);
+  }
+    try {
+const tl = [" cookies 1 ","cookies 2"];
+const cookies = tl[Math.floor(Math.random() * tl.length)];
+      const w = await api.sendMessage("[⚪🔵🔴]𝙄𝙈𝘼𝙂𝙄𝙉𝙀....", event.threadID);
+  
+const response = await axios.get(`https://all-image-genator-d1p.onrender.com/dipto/dalle?prompt=${prompt}&key=dipto008&cookie=${cookies}`)
+      const data = response.data.imgUrls;
+      if (!data || data.length === 0) {
+        api.sendMessage("Empty response or no images generated.",event.threadID,event.messageID);
+      }
+      const diptoo = [];
+      for (let i = 0; i < data.length; i++) {
+        const imgUrl = data[i];
+        const imgResponse = await axios.get(imgUrl, { responseType: 'arraybuffer' });
+        const imgPath = path.join(__dirname, 'assests', `${i + 1}.jpg`);
+        await fs.outputFile(imgPath, imgResponse.data);
+        diptoo.push(fs.createReadStream(imgPath));
+      }
+      await api.unsendMessage(w.messageID);
+      await api.sendMessage({
+  body: `✅ |𝘾𝙍𝙀𝘼𝙏𝙀𝘿 𝙄𝙈𝙂 🔵⚪`,
+        attachment: diptoo
+      },event.threadID, event.messageID);
+    } catch (error) {
+      console.error(error);
+      await api.sendMessage(`Generation failed!\nError: ${error.message}`,event.threadID, event.messageID);
+    }
+  }
+}
